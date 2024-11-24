5958ee62079eb9fbc309c864f4928bcac235ac78
sealmu <seal.mu98@gmail.com>
2024-11-24 21:22:34 +0300

    Исправил ошибку PEP8 isort

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,8 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['84.201.161.153', '127.0.0.1', 'localhost', 'sealmu.sytes.net']
+ALLOWED_HOSTS = ['84.201.161.153', '127.0.0.1',
+                 'localhost', 'sealmu.sytes.net']
 
 
 # Application definition
